8b7cf11538e84b04b50af7da6c19d227ac0da6f2
CalDevC <mr.alexander5132@gmail.com>
2022-03-17 09:48:21 -0700

    Initial Commit

---
+++ Bought-or-Not/LoginVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bought-or-Not
+//
+//  Created by Chase Alexander on 3/17/22.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
