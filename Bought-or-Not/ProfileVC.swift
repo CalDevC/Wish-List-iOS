427b5dc4eb9d40c9578c222c08f6ea430fba79d1
CalDevC <mr.alexander5132@gmail.com>
2022-04-30 23:26:24 -0700

    Friend requests will now disappear after acceptance

--- Bought-or-Not/ProfileVC.swift
+++ Bought-or-Not/ProfileVC.swift
@@ -11,7 +11,7 @@ import Firebase
 class ProfileVC: UIViewController {
     
     @IBOutlet weak var usernameLabel: UILabel!
-    @IBOutlet weak var addFriendBtn: UIButton!
+    @IBOutlet weak var actionBtn: UIButton!
     
     var user: User?
     var currentUser: User?
@@ -27,12 +27,12 @@ class ProfileVC: UIViewController {
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        addFriendBtn.isEnabled = true
-        addFriendBtn.titleLabel?.text = actions[0]
+        actionBtn.isEnabled = true
+        actionBtn.titleLabel?.text = actions[0]
     }
     
-    @IBAction func addFriendBtnPressed(_ sender: UIButton) {
-        addFriendBtn.isEnabled = false
+    @IBAction func actionBtnPressed(_ sender: UIButton) {
+        actionBtn.isEnabled = false
         
         if(actions[0] == "Accept"){
             print("Accepted :)")
@@ -44,99 +44,125 @@ class ProfileVC: UIViewController {
                 return
             }
             
-            db.collection("users").document(currentUID).updateData([
-                "friends": FieldValue.arrayUnion([otherUID])
-            ]) { error in
-                if let error = error {
-                    Util.launchAlert(
-                        senderVC: self,
-                        title: "Error",
-                        message: "Failed to add friend, please try again later :(",
-                        btnText: "ok"
-                    )
-                    self.addFriendBtn.isEnabled = true
-                    print("Error updating document: \(error)")
-                } else {
-                    print("Document successfully updated")
-                    self.navigationController?.popViewController(animated: true)
-                }
-            }
-            
-            db.collection("users").document(otherUID).updateData([
-                "friends": FieldValue.arrayUnion([currentUID])
-            ]) { error in
-                if let error = error {
-                    Util.launchAlert(
-                        senderVC: self,
-                        title: "Error",
-                        message: "Failed to add friend, please try again later :(",
-                        btnText: "ok"
-                    )
-                    self.addFriendBtn.isEnabled = true
-                    print("Error updating document: \(error)")
-                } else {
-                    print("Document successfully updated")
-                    self.navigationController?.popViewController(animated: true)
-                }
-            }
+            self.updateOtherUser(otherUID: otherUID, currentUID: currentUID)
             
         } else if(actions[0] == "Send Friend Request"){
             //Add UID the signed in user's friend list
-            guard let userToNotify = user else{
-                addFriendBtn.isEnabled = true
-                return
+            sendNotification()
+        
+    }
+}
+    
+    func sendNotification(){
+        guard let userToNotify = user else{
+            actionBtn.isEnabled = true
+            return
+        }
+        
+        guard let currentUser = currentUser else{
+            actionBtn.isEnabled = true
+            return
+        }
+        
+        let username = currentUser.username
+        let fullName = currentUser.fullName
+        db.collection("users").document(userToNotify.uid).updateData(
+            ["notifications":
+                FieldValue.arrayUnion(
+                    [[
+                        "message": "\(username) (\(fullName)) has requested to be your friend!",
+                         "sender": [
+                            "uid": currentUser.uid,
+                            "fullName": fullName,
+                            "username": username
+                         ]
+                    ]]
+                )
+            ]
+        ){ error in
+            if let error = error {
+                Util.launchAlert(
+                    senderVC: self,
+                    title: "Error",
+                    message: "Failed to add friend, please try again later :(",
+                    btnText: "ok"
+                )
+                self.actionBtn.isEnabled = true
+                print("Error updating document: \(error)")
+            } else {
+                print("Document successfully updated")
+                self.navigationController?.popViewController(animated: true)
             }
-            
-            guard let currentUser = currentUser else{
-                addFriendBtn.isEnabled = true
-                return
+        }
+    }
+
+    func updateCurrentUser(currentUID: String, otherUser: User){
+        db.collection("users").document(currentUID).updateData([
+            "friends": FieldValue.arrayUnion([otherUser.uid]),
+            "notifications": FieldValue.arrayRemove(
+                [[
+                    "message": "\(otherUser.username) (\(otherUser.fullName)) has requested to be your friend!",
+                     "sender": [
+                        "uid": otherUser.uid,
+                        "fullName": otherUser.fullName,
+                        "username": otherUser.username
+                     ]
+                ]]
+            )
+        ]) { error in
+            if let error = error {
+                Util.launchAlert(
+                    senderVC: self,
+                    title: "Error",
+                    message: "Failed to add friend, please try again later :(",
+                    btnText: "ok"
+                )
+                self.actionBtn.isEnabled = true
+                print("Error updating document: \(error)")
+            } else {
+                print("Document successfully updated")
+                self.navigationController?.popViewController(animated: true)
             }
-            
-            let userToNotifyDocRef = db.collection("users").document(userToNotify.uid)
-            let username = currentUser.username
-            let fullName = currentUser.fullName
-            userToNotifyDocRef.updateData(
-                ["notifications":
-                    FieldValue.arrayUnion(
-                        [[
-                            "message": "\(username) (\(fullName)) has requested to be your friend!",
-                             "sender": [
-                                "uid": currentUser.uid,
-                                "fullName": fullName,
-                                "username": username
-                             ]
-                        ]]
-                    )
-                ]
-            ){ error in
-                if let error = error {
-                    Util.launchAlert(
-                        senderVC: self,
-                        title: "Error",
-                        message: "Failed to add friend, please try again later :(",
-                        btnText: "ok"
-                    )
-                    self.addFriendBtn.isEnabled = true
-                    print("Error updating document: \(error)")
-                } else {
-                    print("Document successfully updated")
-                    self.navigationController?.popViewController(animated: true)
-                }
+        }
+    }
+    
+    func updateOtherUser(otherUID: String, currentUID: String){
+        let otherUserDocRef = db.collection("users").document(otherUID)
+        otherUserDocRef.getDocument { (doc, error) in
+            if let err = error{
+                //TODO: Notify user of error
+                print(err)
+            }
+
+            if let document = doc, document.exists {
+
+                let otherUserData: [String: Any] = document.data()!
+                let otherUser = User(
+                    uid: otherUserData["uid"] as! String,
+                    fullName: otherUserData["fullName"] as! String,
+                    username: otherUserData["username"] as! String
+                )
+                
+                self.updateCurrentUser(currentUID: currentUID, otherUser: otherUser)
             }
         }
         
+        otherUserDocRef.updateData([
+            "friends": FieldValue.arrayUnion([currentUID])
+        ]) { error in
+            if let error = error {
+                Util.launchAlert(
+                    senderVC: self,
+                    title: "Error",
+                    message: "Failed to add friend, please try again later :(",
+                    btnText: "ok"
+                )
+                self.actionBtn.isEnabled = true
+                print("Error updating document: \(error)")
+            } else {
+                print("Document successfully updated")
+                self.navigationController?.popViewController(animated: true)
+            }
+        }
     }
-    
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
