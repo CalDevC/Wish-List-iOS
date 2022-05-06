f975ab77db41e67cbc3a4b5ffbf4785341a94154
CalDevC <mr.alexander5132@gmail.com>
2022-05-05 18:12:41 -0700

    Added box shadows

--- Bought-or-Not/Views/CustomCells/FriendCollectionViewCell.swift
+++ Bought-or-Not/Views/CustomCells/FriendCollectionViewCell.swift
@@ -10,10 +10,42 @@ import UIKit
 class FriendCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var label: UILabel!
     
+    var cornerRadius: CGFloat = 10.0
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        // Apply rounded corners to contentView
+        contentView.layer.cornerRadius = cornerRadius
+        contentView.layer.masksToBounds = true
+        
+        // Set masks to bounds to false to avoid the shadow
+        // from being clipped to the corner radius
+        layer.cornerRadius = cornerRadius
+        layer.masksToBounds = false
+        
+        // Apply a shadow
+        layer.shadowRadius = 8.0
+        
+        //Dark Mode Shadow
+        if(self.traitCollection.userInterfaceStyle == .dark){
+            layer.shadowOpacity = 0.50
+            layer.shadowColor = UIColor.gray.cgColor
+        } else{  //Light Mode Shadow
+            layer.shadowOpacity = 0.30
+            layer.shadowColor = UIColor.black.cgColor
+        }
+        
+        layer.shadowOffset = CGSize(width: 0, height: 5)
     }
-
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        // Improve scrolling performance with an explicit shadowPath
+        layer.shadowPath = UIBezierPath(
+            roundedRect: bounds,
+            cornerRadius: cornerRadius
+        ).cgPath
+    }
+    
 }
