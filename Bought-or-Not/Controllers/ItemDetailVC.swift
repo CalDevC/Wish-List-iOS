ec6271ed21c79b9fdf35231ce1f3e1f7feefff38
CalDevC <mr.alexander5132@gmail.com>
2022-05-12 12:08:41 -0700

    Added activity indicator to image while loading

--- Bought-or-Not/Controllers/ItemDetailVC.swift
+++ Bought-or-Not/Controllers/ItemDetailVC.swift
@@ -10,24 +10,28 @@ import Firebase
 import FirebaseStorage
 
 class ItemDetailVC: UIViewController {
-
+    
     let currentUid = Auth.auth().currentUser!.uid
     let db = Firestore.firestore()
-
+    
     @IBOutlet weak var categoryLabel: UILabel!
     @IBOutlet weak var priceLabel: UILabel!
     @IBOutlet weak var itemDetailImageView: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     private var collectionRef: CollectionReference!
     var item: Item!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.startAnimating()
+        
         self.navigationItem.title = item.name
         categoryLabel.text = item.category
         priceLabel.text = item.price
         
-        getImage(compHandler: reloadItems)
+        getImage()
     }
     
     @IBAction func linkButton(_ sender: Any) {
@@ -35,41 +39,39 @@ class ItemDetailVC: UIViewController {
         UIApplication.shared.open(URL(string: self.item.link)!, options: [:], completionHandler: nil)
     }
     
-    func reloadItems(){
-        print("RELOAD")
-        // activityIndicator.stopAnimating()
-        // self.wishlistTableView.reloadData()
-    }
-    
     func linkTextView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
         UIApplication.shared.open(URL)
         return false
     }
     
-    func getImage(compHandler: @escaping()->Void){
+    func getImage(){
         if(item.image == ""){
             self.itemDetailImageView.image = UIImage(systemName: "photo")
         } else{
             let imageRef = Storage.storage().reference().child(item.image)
             
             imageRef.getData(maxSize: 1 * 100024 * 100024) { data, error in
-              if let error = error {
-                print(error)
-                  Util.launchAlert(
-                    senderVC: self,
-                    title: "Internal Error",
-                    message: "Could not load image 🙁, please try again later",
-                    btnText: "Ok"
-                  )
-              } else {
-                // Data for "images/island.jpg" is returned
-                let itemImage = UIImage(data: data!)
-                self.itemDetailImageView.image = itemImage
-              }
+                if let error = error {
+                    print(error)
+                    self.itemDetailImageView.image = UIImage(systemName: "photo")
+                    Util.launchAlert(
+                        senderVC: self,
+                        title: "Internal Error",
+                        message: "Could not load image 🙁, please try again later",
+                        btnText: "Ok"
+                    )
+                    self.activityIndicator.stopAnimating()
+                    return
+                } else {
+                    // Data for "images/island.jpg" is returned
+                    let itemImage = UIImage(data: data!)
+                    self.itemDetailImageView.image = itemImage
+                    self.activityIndicator.stopAnimating()
+                }
             }
         }
     }
-
+    
 }
 
 extension UIImageView {
