cc5d7d1997ca1cda0a4e747ad4cc5d6a3358327a
CalDevC <mr.alexander5132@gmail.com>
2022-05-11 21:58:09 -0700

    More validation for adding new items

--- Bought-or-Not/Controllers/AddItemVC.swift
+++ Bought-or-Not/Controllers/AddItemVC.swift
@@ -44,9 +44,14 @@ class AddItemVC: UIViewController, UIImagePickerControllerDelegate, UINavigation
     }
     
     func canOpenURL(validationField: UITextField) -> Bool {
-        guard let url = NSURL(string: validationField.text!) else {return false}
-        if !UIApplication.shared.canOpenURL(url as URL) {return false}
-
+        guard let url = NSURL(string: validationField.text!) else {
+            return false
+        }
+        
+        if !UIApplication.shared.canOpenURL(url as URL) {
+            return false
+        }
+        
         let regEx = "((https|http)://)((\\w|-)+)(([.]|[/])((\\w|-)+))+"
         let predicate = NSPredicate(format:"SELF MATCHES %@", argumentArray:[regEx])
         return predicate.evaluate(with: validationField.text)
@@ -61,32 +66,39 @@ class AddItemVC: UIViewController, UIImagePickerControllerDelegate, UINavigation
         print(canOpenURL(validationField: self.itemLink))
         
         // validate required fields or prevent submission
-        if self.itemName.text == "" ||  self.itemPrice.text == "" || self.itemLink.text == "" || canOpenURL(validationField: self.itemLink) == false {
+        if self.itemName.text == "" ||  self.itemPrice.text == "" || self.itemLink.text == ""{
             toggleValidation(validationField: self.itemName)
             toggleValidation(validationField: self.itemPrice)
             toggleValidation(validationField: self.itemLink)
-            if canOpenURL(validationField: self.itemLink) == false {
-                self.itemLink.layer.borderWidth = 1.0
-                self.itemLink.layer.borderColor = UIColor.red.cgColor
-            }
-            else {
-                self.itemLink.layer.borderWidth = 0.0
-            }
-            
+            Util.launchAlert(
+                senderVC: self,
+                title: "Error",
+                message: "Required fields were left empty",
+                btnText: "Ok"
+            )
+            return
+        }else if canOpenURL(validationField: self.itemLink) == false {
+            self.itemLink.layer.borderWidth = 1.0
+            self.itemLink.layer.borderColor = UIColor.red.cgColor
+            Util.launchAlert(
+                senderVC: self,
+                title: "Error",
+                message: "Please enter a valid link",
+                btnText: "Ok"
+            )
             return
         }
-
+        else {
+            self.itemLink.layer.borderWidth = 0.0
+        }
+        
         
         print("List ID is \(listId ?? "nil")")
         
         if self.imageLink != "" {
             uploadMedia(imgName: "\(itemName.text ?? "")_\(listId!)") { (myURL) in
-                // output download URL for image
-                print("Got here with URL: ")
-                print(myURL ?? "nil URL")
                 
-                var ref: DocumentReference? = nil
-                ref = self.db.collection("item").addDocument(data: [
+                self.db.collection("item").addDocument(data: [
                     "listId": self.listId ?? "",
                     "name": self.itemName.text ?? "",
                     "category": self.itemCategory.text ?? "",
@@ -98,12 +110,13 @@ class AddItemVC: UIViewController, UIImagePickerControllerDelegate, UINavigation
                     if let err = err {
                         //Error savng data
                         print("Error adding document: \(err)")
-                        Util.launchAlert(senderVC: self,
-                                         title: "Error",
-                                         message: "Account created but failed to save user data.",
-                                         btnText: "Ok")
+                        Util.launchAlert(
+                            senderVC: self,
+                            title: "Error",
+                            message: "Failed to create new item 🙁, please try again later",
+                            btnText: "Ok"
+                        )
                     } else {
-                        print("Document added with ID: \(ref!.documentID)")
                         // return to previous screen after save
                         self.navigationController?.popViewController(animated: true)
                     }
@@ -124,10 +137,12 @@ class AddItemVC: UIViewController, UIImagePickerControllerDelegate, UINavigation
                 if let err = err {
                     //Error savng data
                     print("Error adding document: \(err)")
-                    Util.launchAlert(senderVC: self,
-                                     title: "Error",
-                                     message: "Account created but failed to save user data.",
-                                     btnText: "Ok")
+                    Util.launchAlert(
+                        senderVC: self,
+                        title: "Error",
+                        message: "Account created but failed to save user data.",
+                        btnText: "Ok"
+                    )
                 } else {
                     print("Document added with ID: \(ref!.documentID)")
                     // return to previous screen after save
@@ -135,7 +150,7 @@ class AddItemVC: UIViewController, UIImagePickerControllerDelegate, UINavigation
                 }
             }
         }
-
+        
     }
     
     @IBAction func AddPhoto(_ sender: UIButton) {
@@ -175,21 +190,21 @@ class AddItemVC: UIViewController, UIImagePickerControllerDelegate, UINavigation
         // Create a reference to the file you want to upload
         let imageRef = storageRef.child("images/\(imgName)")
         imageLink = "images/\(imgName)"
-
+        
         // Upload the file to the path "images/rivers.jpg"
         let uploadTask = imageRef.putFile(from: imageURL!, metadata: nil) { metadata, error in
             guard metadata != nil else {
-            // Uh-oh, an error occurred!
-            return
-          }
+                // Uh-oh, an error occurred!
+                return
+            }
             
             imageRef.downloadURL { (url, error) in
-            guard let downloadURL = url else {
-              // Uh-oh, an error occurred!
-              return
+                guard let downloadURL = url else {
+                    // Uh-oh, an error occurred!
+                    return
+                }
+                completion(downloadURL)
             }
-            completion(downloadURL)
-          }
         }
     }
     
